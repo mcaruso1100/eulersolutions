0b20411381e4923eadd5fa503dff58b16c1b275b
mcaruso1100 <MIke@1992>
2017-02-23 12:09:53 -0500

    finished p7

--- p7.py
+++ p7.py
@@ -5,3 +5,34 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 
 What is the 10 001st prime number?
 '''
+import math
+from itertools import islice
+
+def findPrimeUnderLimit(limit):
+    primes = range(2,limit)
+
+    for n in primes:
+        factors = range(n,limit,n)
+        for x in factors[1:]:
+            print(n)
+            if x in primes:
+                primes.remove(x)
+    return(primes)
+
+def is_prime(x):
+    if x == 2:
+        return True
+    if x < 2 or x%2==0:
+        return False
+    return all(x % i for i in range(3,int(math.sqrt(x)+1),2))
+
+def prime_gen(start=2):
+    x = start
+    while True:
+        if is_prime(x):
+            yield x
+        x += 1
+if __name__=="__main__":
+    print next((islice(prime_gen(),10000,None)))
+
+
